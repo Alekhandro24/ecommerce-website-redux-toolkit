1b05facaa8e5ad2f004561f6fe73cb8dd6ba3b9e
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-27 13:13:22 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import { Home, Category, Cart } from '../pages';
+import Navbar from './Navbar/Navbar';
+import Footer from './Footer/Footer';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template & hello Alekhandro!
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:id" element={<Category />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Footer />
     </div>
   );
 };
+export default App;
