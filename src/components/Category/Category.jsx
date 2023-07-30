627bbb5f11be59eed63ae68c50cf94cc74b66607
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-30 18:11:27 +0300

    Update Category.jsx

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -23,7 +23,7 @@ const Category = ({ categories, status }) => {
               <Link to={`category/${category.id}`} key={category.id}>
                 <div className="category-item">
                   <div className="category-item-img">
-                    <img src={category.image} alt="photoimage" />
+                    <img src={category.image} alt="imagePhoto" />
                   </div>
                   <div className="category-item-name text-center">
                     <h6 className="fs-20">{category.name}</h6>
