461f695912fcea4e727c25416eccd7ea6041b787
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-30 18:05:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import store from 'redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ecommerce-website-redux-toolkit">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/ecommerce-website-redux-toolkit">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
