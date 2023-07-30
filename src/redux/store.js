c6baeee63ebe930ff86c3da5688e85c2f226c775
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-30 18:11:35 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import productReducer from './productSlice';
 import categoryReducer from './categorySlice';
 import modalReducer from './modalSlice';
-import productReducer from './productSlice';
+import cartReducer from './cartSlice';
 
 const store = configureStore({
   reducer: {
+    product: productReducer,
     category: categoryReducer,
     modal: modalReducer,
-    product: productReducer,
+    cart: cartReducer,
   },
 });
 
